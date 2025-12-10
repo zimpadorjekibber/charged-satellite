bcd2b3b94a76c1aeb34ed3b2b7ecbac0f0afb247
Zimpa Dorje <zimpa2@gmail.com>
2025-12-10 15:48:03 +0530

    Permanently fix maps location to Tashi Zom Guest House Kibber

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -178,7 +178,7 @@ export const useStore = create<AppState>()(
                 phone: '+919876543210',
                 secondaryPhone: '+919418612295',
                 whatsapp: '+919876543210',
-                mapsLocation: 'TashiZom+Restaurant,+Kibber'
+                mapsLocation: 'Tashi+Zom+Guest+House,+Kibber,+Spiti'
             },
 
             initialize: () => {
