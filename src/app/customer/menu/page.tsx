1962ec19e3ccd08352fb52370ffc918761ce2bce
Zimpa Dorje <zimpa2@gmail.com>
2025-12-17 12:53:40 +0530

    Enhance YouTube embed permissions for sharing

--- src/app/customer/menu/page.tsx
+++ src/app/customer/menu/page.tsx
@@ -1065,7 +1065,7 @@ export default function MenuPage() {
                                                                                 className="w-full aspect-video"
                                                                                 title="YouTube video player"
                                                                                 frameBorder="0"
-                                                                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                                                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                                                                 allowFullScreen
                                                                             />
                                                                         );
